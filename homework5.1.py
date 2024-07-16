ae25b34a877834a77c59c41310605e48e99bad2e
AMalakhov <spamarkady@yandex.ru>
2024-07-16 11:16:33 +0300

    Update homework5.1.py

--- homework5.1.py
+++ homework5.1.py
@@ -4,11 +4,11 @@ class House:
         self.qty_floors = qty_floors
 
     def go_to(self, new_floor):
-        if self.qty_floors < 1 or self.qty_floors < new_floor:
+        if new_floor < 1 or self.qty_floors < new_floor:
             print('Такого этажа не существует')
         else:
-            for self.qty_floors in range(1, new_floor + 1):
-                print(self.qty_floors)
+            for i in range(1, new_floor + 1):
+                print(i)
 
 h1 = House('ЖК Горький', 18)
 h2 = House('Домик в деревне', 2)
